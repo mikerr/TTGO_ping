3ad4835f9748ddb29127bdbe8800d780b8057f1a
Mike Redrobe <mike@redrobe.net>
2021-04-25 14:59:37 +0100

    Update ttgo_ping.ino

--- ttgo_ping.ino
+++ ttgo_ping.ino
@@ -8,14 +8,11 @@
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
-unsigned long targetTime = 0;
-
 const char* ssid     = "wifi";
 const char* password = "password";
 
-const IPAddress remote_ip(192, 168, 0, 1);
-
 int y = 5;
+
 void printline (String message) {
   tft.setCursor (12, y);
   tft.print(message);
@@ -26,11 +23,18 @@ void printline (String message) {
   }
 }
 
+void clearline () {
+  y -= 10;
+  tft.fillRect(12,y,240,20,TFT_BLACK);
+}
+
+String ip_to_string(IPAddress ip) {
+  char buf[16];
+  sprintf(buf, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3] );
+  return (String(buf));
+}
 void setup() {
 
-  int i=0;
-  char buf[16];
-  
   delay(10);
 
   tft.init();
@@ -39,30 +43,29 @@ void setup() {
   tft.setTextColor(TFT_WHITE);
   
   printline("Connecting to WiFi");
+  
   WiFi.begin(ssid, password);
 
+  int i=0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+
     tft.setCursor (120 + i, 5);
     tft.print(".");
     i += 10;
   }
 
   IPAddress ip = WiFi.localIP();
-  sprintf(buf, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3] );
+  
+  printline("WiFi connected with ip " + ip_to_string(ip));  
 
-  printline("WiFi connected with ip " + String( buf));  
   printline("Pinging ip range");
-
-  ip = remote_ip;
 
   for (i=1;i<255;i++) {
     ip[3] = i;
-    sprintf(buf, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3] );
-    printline(buf);
+    printline(ip_to_string(ip));
     if(!Ping.ping(ip)) {
-        y -= 10;
-        tft.fillRect(12,y,240,20,TFT_BLACK);
+        clearline();
     }
   }
 }
